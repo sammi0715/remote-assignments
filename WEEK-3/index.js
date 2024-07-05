851cfc7e1b2affa1200d3a05ee31bcf6aefa294a
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-07-05 11:21:44 +0800

    change port

--- WEEK-3/index.js
+++ WEEK-3/index.js
@@ -3,7 +3,7 @@ var cookieParser = require("cookie-parser");
 const app = express();
 const path = require("path");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cookieParser());
 app.use(express.static(path.join("WEEK-3")));
