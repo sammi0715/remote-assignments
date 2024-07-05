f30f18560f7802bd6340ea0f91c9cbeb15307ca8
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-07-05 11:07:10 +0800

    change path

--- WEEK-3/index.js
+++ WEEK-3/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 var cookieParser = require("cookie-parser");
 const app = express();
+const path = require("path");
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(cookieParser());
-app.use(express.static("WEEK-3"));
+app.use(express.static(path.join("WEEK-3")));
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello, mySever!</h1>");
