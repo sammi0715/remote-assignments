78ddc802f599b59a18ad1ccfc503607c11819139
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-07-12 17:41:34 +0800

    change router

--- WEEK-4/my-react-app/src/App.js
+++ WEEK-4/my-react-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import PageComponent from "./components/assignment3";
 import CounterComponent from "./components/assignment5"; // 假设你有另一个页面组件
 
