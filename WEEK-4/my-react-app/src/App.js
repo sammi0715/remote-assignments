091f5242023d2d0c63c55e0a28fcb5e80885b85b
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-07-12 10:48:51 +0800

    add react router dom

--- WEEK-4/my-react-app/src/App.js
+++ WEEK-4/my-react-app/src/App.js
@@ -1,21 +1,23 @@
-import "./styles.css";
-import { Navbar, Banner, Welcome, Main, Footer } from "./components/page";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PageComponent from "./components/assignment3";
+import CounterComponent from "./components/assignment5"; // 假设你有另一个页面组件
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+
       <link
         href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
         rel="stylesheet"
       ></link>
-      <header className="App-header">
-        <Navbar />
-      </header>
-      <Banner />
-      <Welcome />
-      <Main />
-      <Footer />
-    </div>
+      <Routes>
+        <Route path="/" element={<><PageComponent /></>} />
+        <Route path="/assignment5" element={
+          <CounterComponent />} />
+      </Routes>
+
+    </Router>
   );
 }
 
